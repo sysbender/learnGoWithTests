e0c22a25b751e155d1f13bc03c953207694c0393
root <root@PREPC156.presagis.com>
2021-07-01 15:52:09 -0400

    hello world

---
+++ hello-world/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const helloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+	return helloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("World", ""))
+}
