95a05def87ab98996e9628c441aadc3621cf6d95
sysbender <sysbender@gmail.com>
2021-07-03 15:30:51 -0500

    JLC-9 #comment pointer and error

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,9 @@
 package jlc09
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -21,6 +24,15 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance + 0
 }
