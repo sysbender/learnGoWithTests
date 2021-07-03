65e0f020b025193958e2c94d224d705a801ac9c4
sysbender <sysbender@gmail.com>
2021-07-03 15:30:51 -0500

    jlc09 #comment -pointer and error

--- pointers/go.mod
+++ pointers/go.mod
@@ -1,3 +1,9 @@
 module jlc09
 
 go 1.16
+
+require (
+	github.com/kisielk/errcheck v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/tools v0.1.4 // indirect
+)
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,22 +1,71 @@
 package jlc09
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
-	got := wallet.Balance()
-	fmt.Printf(" address of the balance in test is %v \n ", &wallet.balance)
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf(" got %s want %s ", got, want)
+		}
 
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf(" got %s want %s ", got, want)
 	}
+
+	assertError := func(t testing.TB, got, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	}
+
+	assertNoError := func(t testing.TB, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+
+	})
+
+	t.Run("withdraw ", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		err := wallet.Withdraw(Bitcoin(10))
+
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+		assertNoError(t, err)
+
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		// withdraw should fail , and balance should not change
+		assertBalance(t, wallet, startingBalance)
+
+	})
 
 }
